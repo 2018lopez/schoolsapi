353fec16c205efd3036c92da616df2ec654ac1d5
Imer Lopez <2018119251@ub.edu.bz>
2022-08-22 13:31:19 -0600

    Set School's api project structure

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+//Filename:  cmd/api/main.go
+
+package main
+
+import (
+	"fmt"
+)
+
+// Application Version number
+const version = "1.0.0"
+
+func main() {
+
+	fmt.Printf("Go Production\n")
+	fmt.Printf(version + "\n")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module schools.ImerLopez.net
+
+go 1.19
