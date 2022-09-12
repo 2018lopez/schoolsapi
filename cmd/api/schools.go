42cf1b3fd5d03a6568ccab7c6659d79df166e322
Imer Lopez <2018119251@ub.edu.bz>
2022-09-12 11:54:48 -0600

    added a school model for our JSON Response

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -5,6 +5,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"schools.ImerLopez.net/internal/data"
 )
 
 //create school handler for "POST" /v1/schools/ endpoint
@@ -24,7 +27,24 @@ func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	//display school id
-	fmt.Fprintf(w, "Schools  %d\n", id)
+	//create a new instance of the school struct containing the id we extracted from our url and some sample data
 
+	school := data.School{
+		ID:       id,
+		CreateAt: time.Now(),
+		Name:     "Apple Tree",
+		Level:    "High School",
+		Contact:  "Anna Smith",
+		Phone:    "601-4412",
+		Address:  "14 Apple Street",
+		Mode:     []string{"blended", "online"},
+		Version:  1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, school, nil)
+
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could processs your request", http.StatusInternalServerError)
+	}
 }
